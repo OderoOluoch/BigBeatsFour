d878ba3ae0dae1204484417b55b02eff31d4026a
OderoOluoch <oderoluoch@gmail.com>
2021-07-15 13:04:19 +0300

    Re-doing the screens - MainSplash

--- app/src/main/java/com/odero/bigtwo/ui/MainActivity.java
+++ app/src/main/java/com/odero/bigtwo/ui/MainActivity.java
@@ -4,10 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.odero.bigtwo.R;
@@ -24,48 +30,55 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
-    RecyclerView recyclerView;
-    ProgressBar progressBar;
-    LinearLayoutManager layoutManager;
-    TrackListApater adapter;
-    List<Result> resultList ;
+    //To control how long the splash screen is displayed.
+    private static int SPLASH_TIME_OUT = 5000;
+
+    //Hooks variables.
+    View first,second,third,fourth,fifth,sixth;
+    TextView tagLine,logoText;
+
+    //Animation variables
+    Animation topAnimation,bottomAnimation,middleAnimation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
-        recyclerView = findViewById(R.id.postRecyclerView);
-        progressBar = findViewById(R.id.progressBar);
-        layoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(layoutManager);
+        topAnimation = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnimation = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+        middleAnimation = AnimationUtils.loadAnimation(this,R.anim.middle_animation);
 
+        //Hooks
+        first = findViewById(R.id.first_line);
+        second = findViewById(R.id.second_line);
+        third = findViewById(R.id.third_line);
+        fourth = findViewById(R.id.fourth_line);
+        fifth = findViewById(R.id.fifth_line);
+        sixth = findViewById(R.id.sixth_line);
 
+        tagLine = findViewById(R.id.tagLine);
+        logoText = findViewById(R.id.textLogo);
 
-        fetchPosts();
-    }
-    private void fetchPosts(){
-        progressBar.setVisibility(View.VISIBLE);
-        TracksClient.getRetrofitClient().getTracks("David").enqueue(new Callback<TrackResponse>() {
+        first.setAnimation(topAnimation);
+        second.setAnimation(topAnimation);
+        third.setAnimation(topAnimation);
+        fourth.setAnimation(topAnimation);
+        fifth.setAnimation(topAnimation);
+        sixth.setAnimation(topAnimation);
+
+        logoText.setAnimation(middleAnimation);
+        tagLine.setAnimation(bottomAnimation);
+
+        //Handle moving from splash screen to welcome screen
+        new Handler().postDelayed(new Runnable() {
             @Override
-            public void onResponse(Call<TrackResponse> call, Response<TrackResponse> response) {
-                if(response.isSuccessful() && response.body() != null){
-                    resultList = response.body().getResults();
-                    Log.e("TAG", resultList.toString());
-
-                    progressBar.setVisibility(View.GONE);
-                    adapter = new TrackListApater(resultList);
-                    recyclerView.setAdapter(adapter);
-
-                    adapter.notifyDataSetChanged();
-                }
+            public void run() {
+                Intent intent = new Intent(MainActivity.this,WelcomeActivity.class);
+                startActivity(intent);
+                finish();
             }
-
-            @Override
-            public void onFailure(Call<TrackResponse> call, Throwable t) {
-                progressBar.setVisibility(View.GONE);
-                Toast.makeText(MainActivity.this,"Error "+ t.getMessage(),Toast.LENGTH_SHORT).show();
-            }
-        });
+        },SPLASH_TIME_OUT);
     }
 }
--- app/src/main/java/com/odero/bigtwo/ui/ResultsActivity.java
+++ app/src/main/java/com/odero/bigtwo/ui/ResultsActivity.java
@@ -1,16 +1,70 @@
 package com.odero.bigtwo.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.odero.bigtwo.R;
+import com.odero.bigtwo.adapter.TrackListApater;
+import com.odero.bigtwo.models.Result;
+import com.odero.bigtwo.models.TrackResponse;
+import com.odero.bigtwo.network.TracksClient;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class ResultsActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    ProgressBar progressBar;
+    LinearLayoutManager layoutManager;
+    TrackListApater adapter;
+    List<Result> resultList ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_results);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.postRecyclerView);
+        progressBar = findViewById(R.id.progressBar);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+
+
+        fetchPosts();
+    }
+    private void fetchPosts(){
+        progressBar.setVisibility(View.VISIBLE);
+        TracksClient.getRetrofitClient().getTracks("David").enqueue(new Callback<TrackResponse>() {
+            @Override
+            public void onResponse(Call<TrackResponse> call, Response<TrackResponse> response) {
+                if(response.isSuccessful() && response.body() != null){
+                    resultList = response.body().getResults();
+                    Log.e("TAG", resultList.toString());
+
+                    progressBar.setVisibility(View.GONE);
+                    adapter = new TrackListApater(resultList);
+                    recyclerView.setAdapter(adapter);
+
+                    adapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<TrackResponse> call, Throwable t) {
+                progressBar.setVisibility(View.GONE);
+                Toast.makeText(ResultsActivity.this,"Error "+ t.getMessage(),Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
