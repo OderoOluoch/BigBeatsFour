aa305fa4109fba069b09a3c3c7d94ccde0f96299
OderoOluoch <oderoluoch@gmail.com>
2021-07-16 00:41:16 +0300

    Started with the Fragment class

--- app/src/main/java/com/odero/bigtwo/models/Result.java
+++ app/src/main/java/com/odero/bigtwo/models/Result.java
@@ -2,7 +2,9 @@ package com.odero.bigtwo.models;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
+import org.parceler.Parcel;
 
+@Parcel
 public class Result {
 
     @SerializedName("wrapperType")
