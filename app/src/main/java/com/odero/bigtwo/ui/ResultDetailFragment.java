16a29c4361724e505a25efecc1b02e5fb1120ac5
OderoOluoch <oderoluoch@gmail.com>
2021-07-16 00:46:24 +0300

    onCReate view

--- app/src/main/java/com/odero/bigtwo/ui/ResultDetailFragment.java
+++ app/src/main/java/com/odero/bigtwo/ui/ResultDetailFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +12,9 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.odero.bigtwo.R;
+import com.odero.bigtwo.models.Result;
+
+import org.parceler.Parcels;
 
 import butterknife.BindView;
 
@@ -25,39 +29,36 @@ public class ResultDetailFragment extends Fragment {
     @BindView(R.id.frtrackNameTextView) TextView mNameLabel;
     @BindView(R.id.frTrackDescriptionTextView) TextView mdescription;
 
-
-
+    private Result mResult;
 
     public ResultDetailFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ResultDetailFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ResultDetailFragment newInstance(String param1, String param2) {
-        ResultDetailFragment fragment = new ResultDetailFragment();
+
+
+    public static ResultDetailFragment newInstance(Result result) {
+        ResultDetailFragment restaurantDetailFragment = new ResultDetailFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+        args.putParcelable("result", Parcels.wrap(result));
+        restaurantDetailFragment.setArguments(args);
+        return restaurantDetailFragment;
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        assert getArguments() != null;
+        mResult = Parcels.unwrap(getArguments().getParcelable("result"));
     }
+
+
+
+
+
+
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
