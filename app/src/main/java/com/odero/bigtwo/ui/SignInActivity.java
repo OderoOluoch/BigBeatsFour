4e9a30f1c4312055f10a2b3b38c8866884590a91
OderoOluoch <oderoluoch@gmail.com>
2021-07-15 12:49:35 +0300

    Re-doing the screens

---
+++ app/src/main/java/com/odero/bigtwo/ui/SignInActivity.java
@@ -0,0 +1,16 @@
+package com.odero.bigtwo.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.odero.bigtwo.R;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
