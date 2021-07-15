13956ee308684c86b2d39c1e7b48472088e81432
OderoOluoch <oderoluoch@gmail.com>
2021-07-16 00:13:07 +0300

    started work on the FRagment

---
+++ app/src/main/java/com/odero/bigtwo/ui/ResultDetailActivity.java
@@ -0,0 +1,16 @@
+package com.odero.bigtwo.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.odero.bigtwo.R;
+
+public class ResultDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result_detail);
+    }
+}
