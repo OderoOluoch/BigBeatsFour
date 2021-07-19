a15a4923b9f8a1a4e803761077e8ff5e8cf14d06
OderoOluoch <oderoluoch@gmail.com>
2021-07-19 18:32:07 +0300

    Logic to log in now in place

--- app/src/main/java/com/odero/bigtwo/ui/WelcomeActivity.java
+++ app/src/main/java/com/odero/bigtwo/ui/WelcomeActivity.java
@@ -7,13 +7,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.odero.bigtwo.R;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class WelcomeActivity extends  AppCompatActivity implements View.OnClickListener {
-
+    FirebaseAuth mAuth;
 
     //Using BIndView from ButterKnife.
     @BindView(R.id.goToLoginScreen)
@@ -24,7 +26,7 @@ public class WelcomeActivity extends  AppCompatActivity implements View.OnClickL
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
-
+        mAuth = FirebaseAuth.getInstance();
         //BindViews
         ButterKnife.bind(this);
 
@@ -35,11 +37,20 @@ public class WelcomeActivity extends  AppCompatActivity implements View.OnClickL
     }
 
     @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser user = mAuth.getCurrentUser();
+        if(user == null){
+            startActivity(new Intent(WelcomeActivity.this, LogInActivity.class));
+        }
+    }
+
+    @Override
     public void onClick(View v) {
         //Helps us navigate between the log in and the register button.
         if(v == login){
-            Intent intent = new Intent(WelcomeActivity.this, LogInActivity.class);
-            startActivity(intent);
+//            Intent intent = new Intent(WelcomeActivity.this, LogInActivity.class);
+//            startActivity(intent);
         }else if(v == register){
             Intent intent = new Intent(WelcomeActivity.this, SignInActivity.class);
             startActivity(intent);
